8de191aeba866c8daa354d808c7b2f572d0e7db4
Dr. Lloyd <lloydtheophilus@gmail.com>
2020-09-15 13:52:36 +0000

    routes

--- app.js
+++ app.js
@@ -19,35 +19,55 @@ app.post("/", function(req, res){
 
   var data = {
     members: [
-      {}
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields:
+        {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
     ]
-  }
+  };
+
+  var jasonData = JSON.stringify(data);
 
   var options = {
     url:"https://us10.api.mailchimp.com/3.0/lists/0f9bc1d883",
     method: "POST",
     headers: {
       "Authorization": "lloyd200 7356386cc55c9f34158f44c5229db949-us10"
-    }
-
-
+    },
+    body: jasonData
 };
 
 
   request(options, function(error, response, body){
     if (error){
-      console.log(error);
+      res.sendFile(__dirname + "/failure.html");
     } else {
-      console.log(response.statusCode);
+      if(response.statusCode ===200){
+        res.sendFile(__dirname + "/success.html");
+      }
+      else{
+        res.sendFile(__dirname + "/failure.html");
+      }
     }
   });
+});
 
+app.post("/failure", function(req, res){
+  res.redirect("/")
+});
 
-})
+app.post("/success", function(req, res){
+  res.redirect("/")
+});
 
 app.listen(3000, function(){
   console.log('server started on port | 3000');
-})
+});
 
 //7356386cc55c9f34158f44c5229db949-us10
 
